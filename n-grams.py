88ac4dc098cd5767bcf156a5dfbc308cb77c9c30
dave1b <57140925+dave1b@users.noreply.github.com>
2021-10-16 19:39:06 +0200

    added main function

--- n-grams.py
+++ n-grams.py
@@ -25,30 +25,33 @@ def remove_punctuations(array):
             array_copy.append(x)
     return array_copy
 
-#Read Shakespeartext remove stopwords and punctuations
-text = nltk.corpus.gutenberg.words('shakespeare-macbeth.txt')
-without_stop_words = remove_stopewords(text)
-without_stop_words_and_punctuations = remove_punctuations(without_stop_words)
-
-#Search for Bi-Grams and print them out
-listBigrams = nltk.bigrams(without_stop_words_and_punctuations)
-freq_bi = nltk.FreqDist(listBigrams)
-for k,v in freq_bi.items():
-    print(k,v)
-
-#Search for Tri-Grams and print them out
-listTrigrams = nltk.trigrams(without_stop_words_and_punctuations)
-freq_tri = nltk.FreqDist(listTrigrams)
-for k,v in freq_tri.items():
-    print(k,v)
-
-
-#Read IMBD-Files and calculate/print the TF-IDF -Values
 def get_tfidf(docs, ngram_range=(2,2), index=None):
     vect = TfidfVectorizer(stop_words="english", ngram_range=ngram_range)
-    tfidf = vect.fit_transform(documents).todense()
+    tfidf = vect.fit_transform(docs).todense()
     return pd.DataFrame(tfidf, columns=vect.get_feature_names(), index=index).T
 
-documents = [open("./ressources/IMDB_1.txt", "r+").read() ,open("./ressources/IMDB_2.txt", "r+").read() ,open("./ressources/IMDB_3.txt", "r+").read() ]
-documents_names = ['Doc {:d}'.format(i) for i in range(len(documents))]
-print(get_tfidf(documents,index=documents_names))
+def main():
+    #Read Shakespeartext remove stopwords and punctuations
+    text = nltk.corpus.gutenberg.words('shakespeare-macbeth.txt')
+    without_stop_words = remove_stopewords(text)
+    without_stop_words_and_punctuations = remove_punctuations(without_stop_words)
+
+    #Search for Bi-Grams and print them out
+    listBigrams = nltk.bigrams(without_stop_words_and_punctuations)
+    freq_bi = nltk.FreqDist(listBigrams)
+    for k,v in freq_bi.items():
+        print(k,v)
+
+    #Search for Tri-Grams and print them out
+    listTrigrams = nltk.trigrams(without_stop_words_and_punctuations)
+    freq_tri = nltk.FreqDist(listTrigrams)
+    for k,v in freq_tri.items():
+        print(k,v)
+
+    #Read IMBD-Files and calculate/print the TF-IDF -Values
+    documents = [open("./ressources/IMDB_1.txt", "r+").read() ,open("./ressources/IMDB_2.txt", "r+").read() ,open("./ressources/IMDB_3.txt", "r+").read() ]
+    documents_names = ['Doc {:d}'.format(i) for i in range(len(documents))]
+    print(get_tfidf(documents,ngram_range=(2,2),index=documents_names))
+
+if __name__ == '__main__':
+    main()
